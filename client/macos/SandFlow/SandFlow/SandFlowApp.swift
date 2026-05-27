6f75ccb0acb3ce4719a873cea7f8ff359a5755df
Y07-py <eruhu07@gmail.com>
2026-05-28 01:32:48 +0900

    first commit

---
+++ client/macos/SandFlow/SandFlow/SandFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandFlowApp.swift
+//  SandFlow
+//
+//  Created by 木本瑛介 on 2026/05/28.
+//
+
+import SwiftUI
+
+@main
+struct SandFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ client/macos/SandFlow/SandFlowTests/SandFlowTests.swift
@@ -0,0 +1,19 @@
+//
+//  SandFlowTests.swift
+//  SandFlowTests
+//
+//  Created by 木本瑛介 on 2026/05/28.
+//
+
+import Testing
+@testable import SandFlow
+
+struct SandFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
